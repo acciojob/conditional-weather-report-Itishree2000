a0504b8c4b0676261d6604193187907ae37d2179
Itishree Jena <itishreej203@gmail.com>
2023-12-08 20:25:53 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,31 @@
-
-// 
-// export default App
 import React from "react";
 import './../styles/App.css'; // Import your CSS file here
 
 const App = () => {
   // Sample weather input
-  const weatherData = { temperature: 25, conditions: "Sunny" };
-  const threshold = 20;
-  const isAboveThreshold = weatherData.temperature > threshold;
+  // const weatherData = { temperature: 25, conditions: "Sunny" };
+  // const threshold = 20;
+  // const isAboveThreshold = weatherData.temperature > threshold;
 
-  return (
-    <div className="container">
-      <h1>Weather Report</h1>
-      <div className="weather-info">
-        <p>Temperature: <span className={isAboveThreshold ? 'above-threshold' : 'below-threshold'}>{weatherData.temperature}°C</span></p>
-        <p>Conditions: {weatherData.conditions}</p>
-      </div>
+  // return (
+  //   <div className="container">
+  //     <h1>Weather Report</h1>
+  //     <div className="weather-info">
+  //       <p>Temperature: <span className={isAboveThreshold ? 'above-threshold' : 'below-threshold'}>{weatherData.temperature}</span></p>
+  //       <p>Conditions: {weatherData.conditions}</p>
+  //     </div>
+  //   </div>
+  // );
+  const weatherData={temperature:25,conditions:"Sunny"};
+  const threshold=20;
+  const isAboveThreshold=weatherData.temperature>threshold;
+  return(
+    <div className="main">
+      <p>Temperature:<span className={isAboveThreshold?'above-threshold':'below-threshold'}>{weatherData.temperature}°C</span></p>
+      <p>condition:{weatherData.conditions}</p>
     </div>
-  );
+  )
 };
 
 export default App;
+
