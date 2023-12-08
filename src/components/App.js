170269a43e9049c41e1ab892ae89e43f2c643f64
Itishree Jena <itishreej203@gmail.com>
2023-12-08 18:34:09 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 
+// import React from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//     </div>
+//   )
+// }
+
+// export default App
 import React from "react";
-import './../styles/App.css';
+import './../styles/App.css'; // Import your CSS file here
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  // Sample weather input
+  const weatherData = { temperature: 25, conditions: "Sunny" };
+  const threshold = 20;
+  const isAboveThreshold = weatherData.temperature > threshold;
 
-export default App
+  return (
+    <div className="container">
+      <h1>Weather Report</h1>
+      <div className="weather-info">
+        <p>Temperature: <span className={isAboveThreshold ? 'above-threshold' : 'below-threshold'}>{weatherData.temperature}°C</span></p>
+        <p>Conditions: {weatherData.conditions}</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
