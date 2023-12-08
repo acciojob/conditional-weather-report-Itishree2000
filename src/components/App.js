b4b24b05c6b932066171308fcd48fa5977fd090e
Itishree Jena <itishreej203@gmail.com>
2023-12-08 18:43:52 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,5 @@
 
-// import React from "react";
-// import './../styles/App.css';
-
-// const App = () => {
-//   return (
-//     <div>
-//         {/* Do not remove the main div */}
-//     </div>
-//   )
-// }
-
+// 
 // export default App
 import React from "react";
 import './../styles/App.css'; // Import your CSS file here
